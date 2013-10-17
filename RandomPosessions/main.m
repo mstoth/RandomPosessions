bf99f56e9d9be3e06445eb3c7c7d7a2e002e66c4
Michael Toth <michael@virtualpianist.com>
2013-10-17 08:15:48 -0400

    Initial Commit

---
+++ RandomPosessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPosessions
+//
+//  Created by Michael Toth on 10/17/13.
+//  Copyright (c) 2013 Michael Toth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
