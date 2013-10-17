28ccd8e284e587bc5c09459f81638a09343614c6
Michael Toth <michael@virtualpianist.com>
2013-10-17 08:20:04 -0400

    Added BNRItem

---
+++ BNRItem.h
@@ -0,0 +1,13 @@
+//
+//  BNRItem.h
+//  RandomPosessions
+//
+//  Created by Michael Toth on 10/17/13.
+//  Copyright (c) 2013 Michael Toth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+
+@end
---
+++ BNRItem.m
@@ -0,0 +1,13 @@
+//
+//  BNRItem.m
+//  RandomPosessions
+//
+//  Created by Michael Toth on 10/17/13.
+//  Copyright (c) 2013 Michael Toth. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
+@end
